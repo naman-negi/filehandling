28f8fec7a2d986df594659d051e22126559eb092
Naman <namanv2@gmail.com>
2021-07-15 16:45:35 +0530

    Commit 1

---
+++ src/mainfunction.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.util.Scanner;
+class school{
+	String addclass;
+	school(String addclass){
+		
+	}
+	
+	void classadd(){
+		
+	}
+	
+	sortstudentfile(){
+		
+	}
+	
+	deletestudentfile({){
+		
+	}
+}
+
+
+
+public class mainfunction {
+	public static void main(String[] args) {
+		String path="C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/";
+		Scanner sc1= new Scanner(System.in);
+		path =path+sc1.next();
+		File sc2 = new File(path);
+		sc2.mkdir();
+	}
+}
