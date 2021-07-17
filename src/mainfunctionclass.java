1b2db1e34eb24b281e48cd132a942343115c1c89
Naman <namanv2@gmail.com>
2021-07-17 21:50:03 +0530

    Commit 11

--- src/mainfunctionclass.java
+++ src/mainfunctionclass.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.util.Scanner;
 
@@ -5,60 +6,68 @@ import java.util.Scanner;
 class mainfunctionclass {
 		public static void main(String[] args) {
 		String path,str,str2;
-		String[] str3;
-		Scanner firstinput = new Scanner(System.in);
-	    File check;
 	    school calling = new school();
 		int choice;
 		do {
 			path="C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/student/";
 			calling.listdirectoryorfiles(path);
 			System.out.println("Which directory you want to see(enter the appropriate name):");
-			str2=firstinput.nextLine();
-			path=path+str2+"/"; 
-			calling.listdirectoryorfiles(path);
+			Scanner firstinput = new Scanner(System.in);
+			str=firstinput.nextLine();
+			path=path+str; 
+			File file = new File(path);
 			do {
 				System.out.println("1.You want to add a file");
 				System.out.println("2.You want to delete a file");
 				System.out.println("3.You want to search a file");
+				System.out.println("Type anything else if you dont want to do any of the above:");
+				System.out.println("Please enter with appropriate serial number:");
 				choice=firstinput.nextInt(); 
 					   if (choice==1) {//to add a file in a directory
-						    check = new File(path);
-						    str3=check.list();
-						   if (str3==null) {
-							   System.out.println("No files in the current dierctory add new file name:");
-							   System.out.println("Name the file you wan to add:");
-							   str=firstinput.nextLine();
-							   calling.addstudents(str,path);
-						   }
-						   else {
+						   System.out.println("This is the following list of files");
+						   calling.listdirectoryorfiles(path);
+						   if (file.list().length>0) {
 							   System.out.println("Name the file you want to add:");
-							   str=firstinput.nextLine();
-							   calling.addstudents(str,path);
+							   Scanner sixthinput = new Scanner(System.in);
+							   str2=sixthinput.nextLine();
+							   calling.addstudents(str2,path+"/");  
+						   }
+						  else {
+							  System.out.println("No files in the current dierctory add new file name:");
+							   System.out.println("Name the file you wan to add:");
+							   Scanner secondinput = new Scanner(System.in);
+							   str2=secondinput.nextLine();
+							   calling.addstudents(str2,path+"/");
 						   }
 					   }
-					   if (choice==2 ) {//to delete a file
+					   else if (choice==2 ) {//to delete a file
 						   calling.listdirectoryorfiles(path);//this will list file in sorted before a particular is deleted
 						   System.out.println("Please enter name of the file you wwant to delete:");
-						   str2=firstinput.nextLine();
-					       calling.filedeleted(str2);
+						   Scanner thirdinput = new Scanner(System.in);
+						   str=thirdinput.nextLine();
+					       calling.filedeleted(str);
 						   System.out.println("Updated list file after deletion:");
 					       calling.listdirectoryorfiles(path);
 					       }
-					   if (choice==3) {//to search a file
+					   else   if (choice==3) {//to search a file
 						   calling.listdirectoryorfiles(path);//display in  file sorted manner before some file is searched
 						   System.out.println("Please enter name of the file you wwant to search:");
-						   str2=firstinput.nextLine();
-					       str2=calling.searchfile(str2);
-						   System.out.println(str2);
+							Scanner fourthinput = new Scanner(System.in);
+						   str=fourthinput.nextLine();
+					       str=calling.searchfile(str);
+						   System.out.println(str);
 					   }
-					   System.out.println("Type exit if you want to exit this execution  context or type no:");
-					   str2=firstinput.nextLine();
-					   }while(str2!="exit");
-			System.out.println("Type exit if you want to exit or type no:");
-			str2=firstinput.nextLine();
-			}while(str2!="exit");
-		firstinput.close();
+					   else { 
+					   System.out.println("Do you want to exit this directory:");
+					   Scanner fifthinput = new Scanner(System.in);
+					   str=fifthinput.nextLine();
+					   }
+					   }while(str.equals("no"));
+			
+			System.out.println("To exit the application type exit or if you want to keep browsing other directories type no:");
+			Scanner seventhinput = new Scanner(System.in);
+			str=seventhinput.nextLine();
+			}while(str.equals("no"));
 		}
 }
 		
--- src/school.java
+++ src/school.java
@@ -1,108 +1,79 @@
-import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.File;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.*;
-
-class school{
-	
+public class school{
 	String name;
 	String rollno;
-	
 	//This Function will add new txt files in a new directory and will also display files
-	void addstudents(String str,String path){
-		str=path+str;
-		FileWriter fw1=null;;  
+	public void addstudents(String str,String path) {
+		str=path+str+".txt";
+		Scanner input1= new Scanner (System.in);
 		try { 
-			fw1=new FileWriter(str);
-			BufferedWriter bf= new BufferedWriter(fw1);
+			FileWriter fw1=new FileWriter(str);
 			System.out.println("Please enter the name of the student:");		
-			Scanner input1= new Scanner (System.in);
 			name=input1.nextLine();
-			bf.write("Name: "+name);
+			fw1.write("Name: "+name);
 			System.out.println("Please enter the roll no:");
-			Scanner input2=new Scanner(System.in);
-			rollno=input2.nextLine();
-			bf.write("Roll No: "+rollno);
-			bf.close();
-			input1.close();
-			input2.close();
+			rollno=input1.nextLine();
+			fw1.write("Roll No: "+rollno);
+			fw1.close();
 		}catch(IOException e){
 			e.printStackTrace();
-		}finally {if (fw1 != null) {
-	        try {
-	            fw1.close();
-	           
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	        }
-		    listdirectoryorfiles(str);//will display sorted files 
-			System.out.println("Want to add more files in this direcotry(Yes/no):");
-			Scanner adding = new Scanner(System.in);
-			str=adding.nextLine();
-			Scanner student1=null;
-			if (str=="yes") {
-				System.out.println("Please enter the name of the file needs to be added:");		
-				student1 = new Scanner(System.in);
-				str=student1.nextLine();
-				addstudents(str,path);
-			}
-			adding.close();
-			student1.close();
 		}
-	}
-	
+		    System.out.println("The Updated List");
+		    listdirectoryorfiles(path);//will display sorted files 
+			System.out.println("Want to add more files in this direcotry(Yes/no):");
+			str=input1.nextLine();
+			if (str.equals("yes")) {
+				System.out.println("Please enter the name of the file needs to be added:");		
+				//Scanner student1 = new Scanner(System.in);
+				String str2=input1.nextLine();
+				str=str2;
+				addstudents(str,path);	
+			}
+			
+		}
 	//list directories or files in sorted manner
-	void listdirectoryorfiles(String path) {
-		
-	    File folder = new File(path);
-		File[] listOfFiles = folder.listFiles();
-		List<String> files = new ArrayList<String>();
-		for (int i = 0; i < listOfFiles.length; i++) {
-			   if (listOfFiles[i].isFile()) {
-			     files.add(listOfFiles[i].getName());
-			   }
-			   }
-		Collections.sort(files);
-		System.out.println(files);//This will display sorted directory or files
+	public void listdirectoryorfiles(String path) {
+		   File directorypath = new File(path);
+		   File listFiles[] = directorypath.listFiles();
+		   List<String> files = new ArrayList<String>();
+		   for (int i = 0; i < listFiles.length; i++) {
+					   files.add(listFiles[i].getName()); 
+				}
+				Collections.sort(files);
+				System.out.println(files);
 	}
-	
 	//search a particular file in all directories
-	String searchfile(String str) {
-		File folder = new File("C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/class1/");
+	public String searchfile(String str) {
+		str=str+".txt";
+		File folder = new File("C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/student/class1/");
 		File[] listOfFiles = folder.listFiles();
 		for (int i = 0; i < listOfFiles.length; i++) {
-			   if (listOfFiles[i].isFile()) {
-			     if (str==listOfFiles[i].getName()) {
-			    	 return "Item found at position: "+i+1+"In the directory class1";
+			     if (str.equals(listOfFiles[i].getName())) {
+			    	 str="Item found In the directory class1";
+			    	 }
     }
-    }			     
-	}
- 
-		folder = new File("C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/class2/");
+		folder = new File("C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/student/class2/");
 		listOfFiles = folder.listFiles();
 		for (int i = 0; i < listOfFiles.length; i++) {
-			   if (listOfFiles[i].isFile()) {
-			     if (str==listOfFiles[i].getName()) {
-			    	return "Item found at position: "+i+1+" In the directory class2";
-			     }
+			if (str.equals(listOfFiles[i].getName())) {
+		    	 str="Item found In the directory class2";   	 
+		}
+		}
+		 return str;
 	}
-	}
-		return null;
-	}
-    
 	//Delete a particular file
-	void filedeleted(String str){
+	public void filedeleted(String str){
    
-    	String path="C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/student/class1/"+str;
+    	String path="C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/student/class1/"+str+".txt";
     	File file=new File(path);
     	if (file.isFile()) {
     		file.delete();
     		}
     	else {
-    		path="C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/student/class2/"+str;
+    		path="C:/Users/Naman/Desktop/eclipse-workspace/filehandling/src/student/class2/"+str+".txt";
     	    file = new File(path);
     	    file.delete();
     	}
